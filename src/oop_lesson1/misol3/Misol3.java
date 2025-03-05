80dc7cd31a6c58b0b65ad33557565e6118ee3c3a
Salohiddin00 <salohiddinabduganiyev869@gmail.com>
2025-03-05 09:23:46 +0500

    add

---
+++ src/oop_lesson1/misol3/Misol3.java
@@ -0,0 +1,17 @@
+package oop_lesson1.misol3;
+
+public class Misol3 {
+    public static void main(String[] args) {
+        Accaunt account = new Accaunt(1122, 20000);
+
+        account.setAnnualInterestRate(4.5);
+
+        account.withdraw(2500);
+
+        account.deposit(3000);
+
+        System.out.printf("Balans: $%.2f\n", account.getBalance());
+        System.out.printf("Oylik foiz stavkasi: %.2f%%\n", account.getMonthlyInterestRate());
+        System.out.printf("Oylik foiz: $%.2f\n", account.getMonthlyInterest());
+    }
+}
